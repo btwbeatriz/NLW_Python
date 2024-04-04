7372cf557e331249d34554c077b40139db192c2a
btwbeatriz <beatriz.ffreitas@hotmail.com>
2024-04-04 13:01:02 -0300

    initial commit

---
+++ src/models/settings/connection.py
@@ -0,0 +1,30 @@
+import mariadb
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class __DBConnectionHandler:
+    def __init__(self) -> None:
+        self.__connection_string = "mysql+pymysql://root:root@host/storage.db"
+        self.__engine = None
+        self.session = None
+    
+    def connect_to_db(self) -> None:
+        try:
+            self.__engine = create_engine(self.__connection_string)
+            self.session = sessionmaker(bind=self.__engine)()
+            print("Connected to MariaDB!")
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB: {e}")
+
+    def get_engine(self):
+        return self.__engine    
+    
+    def __enter__(self):
+        session_maker = sessionmaker()
+        self.session = session_maker(bind=self.__engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+
+db_connection_handler = __DBConnectionHandler()
